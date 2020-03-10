b0d331bbaaef79cbc1f3bc3eda9ed5d87ecd76ad
BogdanAntonyuk <boba.tochka@gmail.com>
2020-03-10 20:34:31 +0200

    half of 104

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,53 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
 
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email:{
-		type: String,
-		require: true,
-		trim: true,
-		lowercase: true,
-		validate(value){
-			if(!validator.isEmail(value)){
-				throw new Error('Email is not valid')
-			}
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value){
-			if (value<0){
-				throw new Error('Age must be positive number')
-			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 7,
-		trim: true,
-		validate(value){
-			if(value.includes('password')){
-				throw new Error('Password could not contain \'password\'!')
-			}
-		}
-	}
+const userSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+      trim: true,
+   },
+   email: {
+      type: String,
+      require: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+         if (!validator.isEmail(value)) {
+            throw new Error('Email is not valid');
+         }
+      },
+   },
+   age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+         if (value < 0) {
+            throw new Error('Age must be positive number');
+         }
+      },
+   },
+   password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+         if (value.includes('password')) {
+            throw new Error("Password could not contain 'password'!");
+         }
+      },
+   },
 });
+
+userSchema.pre('save', async function (next){
+	const user = this;
+
+	console.log('Just before saving!');
+
+	next();
+})
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
