f94e7d207e939a2af8b5d1e9c5c0b2e946f29e98
BogdanAntonyuk <boba.tochka@gmail.com>
2020-03-12 14:06:54 +0200

    112-done

--- src/models/user.js
+++ src/models/user.js
@@ -1,52 +1,102 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-   name: {
-      type: String,
-      required: true,
-      trim: true,
-   },
-   email: {
-      type: String,
-      require: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-         if (!validator.isEmail(value)) {
-            throw new Error('Email is not valid');
-         }
-      },
-   },
-   age: {
-      type: Number,
-      default: 0,
-      validate(value) {
-         if (value < 0) {
-            throw new Error('Age must be positive number');
-         }
-      },
-   },
-   password: {
-      type: String,
-      required: true,
-      minlength: 7,
-      trim: true,
-      validate(value) {
-         if (value.includes('password')) {
-            throw new Error("Password could not contain 'password'!");
-         }
-      },
-   },
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+		require: true,
+		trim: true,
+		lowercase: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Email is not valid');
+			}
+		},
+	},
+	age: {
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be positive number');
+			}
+		},
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 7,
+		trim: true,
+		validate(value) {
+			if (value.includes('password')) {
+				throw new Error("Password could not contain 'password'!");
+			}
+		},
+	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				required: true,
+			},
+		},
+	],
 });
 
-userSchema.pre('save', async function (next){
+userSchema.methods.toJSON = function() {
+	const user = this;
+	const userObject = user.toObject();
+
+	delete userObject.password;
+	delete userObject.tokens; 
+
+	return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function() {
+	const user = this;
+	const token = jwt.sign({ _id: user._id }, 'thisismynewcourse');
+
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+
+	return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new Error('Unable to login(mail)');
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password);
+
+	if (!isMatch) {
+		throw Error('Unable to login(pass)');
+	}
+
+	return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function(next) {
 	const user = this;
 
-	console.log('Just before saving!');
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
 
 	next();
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
